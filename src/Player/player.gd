e3d703c05ecc573b190493a772b7a6dc9daf2c9b
Bard-Gaming <christophe@dronne.fr>
2024-04-08 20:49:15 +0200

    Fixed** player movement process

--- src/Player/player.gd
+++ src/Player/player.gd
@@ -1,32 +1,20 @@
 extends CharacterBody3D
 
-# How fast the player moves in meters per second.
 @export var speed = 14
-# The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
 var target_velocity = Vector3.ZERO
 
 
 func _physics_process(delta):
-	var direction = Vector3.ZERO
-
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		#$Pivot.basis = Basis.looking_at(direction)
+	var direction = Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_back") - Input.get_action_strength("move_forward")
+	).normalized()
 
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+	target_velocity.z = direction.y * speed
 
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
