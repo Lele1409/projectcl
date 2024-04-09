c978fd576dae861b107b8e9bf13fc1f745ad4ed3
Bard-Gaming <christophe@dronne.fr>
2024-04-09 14:02:09 +0200

    [Components] Added Attack component

--- src/Scripts/Nodes/attack.gd
+++ src/Scripts/Nodes/attack.gd
@@ -1,6 +1,24 @@
 extends Node
 class_name AttackNode
 
+## Node the attack originates from
+@export var attacker: Node
+## Amount of damage (on avg) the attack should do
+@export var damage: float = 5
+## Maximum possible variance to damage. If 0, the damage will always be the same
+@export var damage_variance: float = 0.5
+## Knockback the attack is supposed to inflict on target
+@export var knockback: float
+
+
+# --------- Getters and Setters --------- :
+func get_attacker() -> Node:
+	return attacker
+
 
 func get_damage() -> float:
-	return 1
+	return damage + (randf_range(-damage_variance, damage_variance))
+
+
+func get_knockback() -> float:
+	return knockback
