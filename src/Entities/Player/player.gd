3b451bd2c3f039db4ead6d8e4add5bbf0c1c289e
Bard-Gaming <christophe@dronne.fr>
2024-04-09 14:49:31 +0200

    [Entities] updated player movement (made code cleaner & added delta to player movement to help with potential lag issues)

--- src/Entities/Player/player.gd
+++ src/Entities/Player/player.gd
@@ -1,25 +1,33 @@
 extends CharacterBody3D
 
-@export var speed = 14
+## Movement speed of player
+@export var speed = 8
+## Fall acceleration, applied every physics frame (multiplied by ~1/60)
 @export var fall_acceleration = 75
 
 var target_velocity = Vector3.ZERO
 
 
 func _physics_process(delta):
-	var direction = Vector2(
+	print(delta)
+	
+	# Handle character movement
+	update_target_velocity(delta)
+	velocity = target_velocity
+	move_and_slide()
+
+
+func update_target_velocity(delta) -> void:
+	var movement_direction = Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		Input.get_action_strength("move_back") - Input.get_action_strength("move_forward")
 	).normalized()
+	
+	var movement_adjust = speed * (delta * 60)  # delta should be around 1/60, so delta*60 should be 1
 
-	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.y * speed
+	target_velocity.x = movement_direction.x * movement_adjust
+	target_velocity.z = movement_direction.y * movement_adjust
 
-	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+	# apply gravity if not grounded
+	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-
-	# Moving the Character
-	velocity = target_velocity
-	move_and_slide()
